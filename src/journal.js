41b105d3eb555b63bd044bd47600e8bc3c456132
Masen Matthews and Nico Daunt <masen@>
2018-03-12 13:11:05 -0700

    working code

--- src/journal.js
+++ src/journal.js
@@ -1,10 +1,34 @@
-export function journal(entry) {
-  function Entry(title, body) {
-    this.title = title;
-    this.body = body;
+
+export function numberOfWords (entry) {
+  return entry.split(" ").length;
+}
+
+export function numberOfVowels(entry) {
+  var count = 0;
+
+  for (var i = 0; i < entry.length; i++) {
+    if ("aeiouAEIOU".includes(entry[i])) {
+      count++;
+    }
   }
 
-  var output = [];
+  return count;
+}
 
-  return output;
+export function numberOfConsts(entry) {
+  var count = 0;
+
+  for (var i = 0; i < entry.length; i++) {
+    if (!"aeiouAEIOU ".includes(entry[i])) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+export function getTeaser(entry) {
+  var words = entry.split(" ");
+  words.splice(8);
+  return words.join(' ');
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import { journal } from './journal';
 import './styles.css';
+import { numberOfWords, numberOfVowels, numberOfConsts, getTeaser} from './journal';
 
 $(document).ready(function() {
   $('#journal-form').submit(function(event) {
     event.preventDefault();
-    var output = journal(entry);
+    var entry = $('#entry').val();
+    var output1 = numberOfWords(entry);
+    var output2 = numberOfVowels(entry);
+    var output3 = numberOfConsts(entry);
+    var output4 = getTeaser(entry);
+    $('#solution').append("<li>words : " + output1 + "</li>");
+    $('#solution').append("<li>vowels: " + output2 + "</li>");
+    $('#solution').append("<li>consts: " + output3 + "</li>");
+    $('#solution').append("<li>teaser: " + output4 + "</li>");
+  });
 });
